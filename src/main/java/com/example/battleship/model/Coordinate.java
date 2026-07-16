fc9cc2c73ac5d58479a66ad9ab2830e651f58e4e
Leo0208r <roseroalexis04@gmail.com>
2026-07-15 19:02:32 -0500

    added logic in Coordinate class and created an exception

--- src/main/java/com/example/battleship/model/Coordinate.java
+++ src/main/java/com/example/battleship/model/Coordinate.java
@@ -1,4 +1,40 @@
 package com.example.battleship.model;
 
+import com.example.battleship.model.exceptions.InvalidCoordinateException;
+
 public class Coordinate {
+    private final int row;
+    private final int col;
+    public Coordinate(int row, int col){
+        if(validate(row,col)){
+            this.row=row;
+            this.col=col;
+        }else{
+            throw new InvalidCoordinateException("Row or Col outside the table", row ,col);
+        }
+    }
+    public int getRow(){return row;}
+    public int getCol(){return col;}
+    private boolean validate(int row, int col){
+        return row>=0 && row<=9 && col>=0 && col<=9;
+    }
+    @Override
+    public String toString(){
+        char columnLetter=(char)('A'+col);
+        String stringColumnLetter=String.valueOf(columnLetter);
+        return stringColumnLetter+(row+1);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this==obj){
+            return true;
+        }
+        if(!(obj instanceof Coordinate other)){return false;}
+        return row==other.row && col==other.col;
+    }
+    @Override
+    public int hashCode(){
+        return 31*row+col;
+    }
+
 }

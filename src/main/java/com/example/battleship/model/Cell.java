2a8188f10c94a4a9588c55a7f9c81b7c9c7711a4
Leo0208r <roseroalexis04@gmail.com>
2026-07-16 08:38:06 -0500

    added logic in cell class

--- src/main/java/com/example/battleship/model/Cell.java
+++ src/main/java/com/example/battleship/model/Cell.java
@@ -1,4 +1,16 @@
 package com.example.battleship.model;
 
+import com.example.battleship.model.enums.CellState;
+
 public class Cell {
+    private CellState cellState;
+    private Coordinate coordinate;
+    public Cell(Coordinate coordinate){
+        this.coordinate=coordinate;
+        cellState=CellState.EMPTY;
+    }
+    public CellState getCellState(){return cellState;}
+    public void setCellState(CellState cellState){this.cellState=cellState;}
+    public Coordinate getCoordinate(){return coordinate;}
+
 }

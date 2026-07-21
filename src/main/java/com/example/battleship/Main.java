ed11f2d467a35c5163bbf2c8ba9d6dd9daa6e1e0
JulioCesar <urbanojulio89@gmail.com>
2026-07-20 20:27:27 -0500

    feat: integracion de vistas fxml, css y SceneManager

--- src/main/java/com/example/battleship/Main.java
+++ src/main/java/com/example/battleship/Main.java
@@ -1,16 +1,18 @@
 package com.example.battleship;
 
+import com.example.battleship.view.SceneManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        SceneManager.getInstance().setStage(primaryStage);
+
+        SceneManager.getInstance().changeScene("menu-view.fxml");
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
     }
 }

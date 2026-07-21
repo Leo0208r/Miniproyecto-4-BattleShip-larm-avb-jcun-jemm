b39459f463ece0f6f34106f9f3facd2a19b29446
AlejandroVB-42 <alejandro.velez.bejarano@correounivalle.edu.co>
2026-07-21 16:32:45 -0500

    fix error

--- src/main/java/com/example/battleship/controller/EndController.java
+++ src/main/java/com/example/battleship/controller/EndController.java
@@ -2,16 +2,22 @@ package com.example.battleship.controller;
 
 import com.example.battleship.view.SceneManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+@SuppressWarnings("unused")
 public class EndController {
+
+    @FXML private Label lblTitle;
+    @FXML private Label lblPlayerNickname;
+    @FXML private Label lblEnemyShipsSunk;
+    @FXML private Label lblPlayerMissedShots;
+    @FXML private Label lblPlayerShipsSunk;
+    @FXML private Label lblEnemyMissedShots;
+    @FXML private Button btnMainMenu;
 
     @FXML
     private void onRestartButtonClick() {
         SceneManager.getInstance().changeScene("menu-view.fxml");
-    }
-
-    @FXML
-    private void onExitButtonClick() {
-        System.exit(0);
     }
 }
